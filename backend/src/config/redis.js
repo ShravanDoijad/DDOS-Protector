141bb5dd82c5b679be1a7392fccf1814b8abbd65
shravandoijad <shravandoijad4@gmail.com>
2026-05-04 21:27:29 +0530

    failed solve 2

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,17 +1,8 @@
-import { Redis } from '@upstash/redis'
+const { Redis } = require('@upstash/redis');
+
 const redisClient = new Redis({
-  url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
-})
-
-
-redisClient.on('connect', () => {
-    console.log('Connected to Redis successfully');
+    url: process.env.REDIS_URL,
+    token: process.env.REDIS_TOKEN,
 });
-
-redisClient.on('error', (err) => {
-    console.error('Redis connection error:', err);
-    process.exit(1);
-}); 
 
 module.exports = redisClient;
